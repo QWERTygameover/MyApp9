3fc511d73d02b9b6a9e9f0c6638088403b6dd846
QWERTygameover <my-email@ya.ru>
2023-06-07 00:36:00 +0700

    12

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button button1 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:-0.45609946,-90.26607513"));
+                startActivity(intent);
+            }
+        });
+    }
+}
